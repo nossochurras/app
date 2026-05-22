7c32e35cb375c5c217c0e02cee5ef09976346720
nossochurras <nossochurras.app@gmail.com>
2026-05-21 22:35:35 -0300

    Create supabaseClient.ts

---
+++ src/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://slnjcoapvlosorqiftxa.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsbmpjb2Fwdmxvc29ycWlmdHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzOTMzNzYsImV4cCI6MjA5NDk2OTM3Nn0.HIPXGmjWEwkzfJdIGSi2DB_RcgnWBPkEHGodHd-OeAY'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
